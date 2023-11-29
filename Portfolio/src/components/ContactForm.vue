b243ba7a39c5147f2ecedfe6c8212fc2904e3b6d
Julius42D <julius.buller@42digital.de>
2023-11-29 14:59:28 +0100

    Added email form + functionality; Added button for conditional rendering email form

---
+++ Portfolio/src/components/ContactForm.vue
@@ -0,0 +1,48 @@
+<script setup>
+import emailjs from 'emailjs-com';
+import {ref} from 'vue';
+
+const form = ref(null);
+const inputFieldReset = ref(null);
+
+const mailData = {
+    SERVICE_ID: 'service_8g4fnzo',
+    TEMPLATE_ID: 'template_9vbjw1a',
+    PUBLIC_KEY: '-bWoGd2tE8SGlNllE',
+}
+    
+
+const sendMail = () => {
+  emailjs.sendForm(mailData.SERVICE_ID, mailData.TEMPLATE_ID , form.value, mailData.PUBLIC_KEY)
+    .then(() => {
+      alert('Message sent!');
+      inputFieldReset.value = " ";
+    })
+    .catch((error) => {
+      alert('Message not sent', error);
+    });
+};
+</script>
+
+<template>
+        <div class="form-container">
+            <form class="form" ref="form" @submit.prevent="sendMail">
+                <div class="form-group">
+                    <input name="from_name" placeholder="Name" type="text" :value="inputFieldReset" required>
+                </div>
+                <div class="form-group">
+                    <input name="email" placeholder="E-Mail" type="email" :value="inputFieldReset" required>
+                </div>
+                <div class="form-group">
+                    <input name="subject" placeholder="Subject" type="text" :value="inputFieldReset" required>
+                </div>
+                <div class="form-group">
+                    <textarea class="text-area" name="message" placeholder="Message" type="text" :value="inputFieldReset"
+                        required></textarea>
+                </div>
+                <div class="form-group">
+                    <input class="submit" type="submit" name="send" />
+                </div>
+            </form>
+        </div>
+</template> 
