aa96ab803e6ac7b89e61ba1871fb5d4f801c5aea
Julius42D <julius.buller@42digital.de>
2024-02-01 16:12:45 +0100

    Tweaked responsive design

--- Portfolio/src/views/ProjectsView.vue
+++ Portfolio/src/views/ProjectsView.vue
@@ -1,7 +1,13 @@
+<script setup>
+const constructionGif = '/images/construction.gif';
+
+</script>
+
 <template>
     <main class="home-page">
         <div class="personal-wrapper">
             <div class="name">Coming Soon!</div>
+            <img class="construction-gif" :src="constructionGif">
         </div>
     </main>
 </template>
